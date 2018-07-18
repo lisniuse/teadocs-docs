933d111874876ad33db4a6c78bec0bb463f96f5e
lisniuse <17560235@qq.com>
2018-07-18 17:16:12 +0800

    update.

--- cn/teadocs.config.js
+++ cn/teadocs.config.js
@@ -16,7 +16,7 @@ module.exports = {
         headHtml: "",
         footHtml: "",
         isMinify: true,
-        rootPath: "/en/"
+        rootPath: "/cn/"
     },
     nav: {
         tree: "./tree"
